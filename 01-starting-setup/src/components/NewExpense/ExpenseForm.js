b1b3e53ee9253d188117c13202ff9fda05b9e395
Victor <victormovileanu@gmail.com>
2022-07-20 22:25:11 +0200

    add form submit handler

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -18,19 +18,31 @@ function ExpenseForm() {
     setEnteredDate(evt.target.value)
   }
 
-  return <form action="">
+  function submitHandler(evt) {
+    evt.preventDefault()
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    setEnteredTitle('')
+    setEnteredDate('')
+    setEnteredAmount('')
+  }
+
+  return <form action="" onSubmit={submitHandler}>
     <div className={'new-expense__controls'}>
       <div className={'new-expense__control'}>
         <label htmlFor={'title'}>Title</label>
-        <input name={'title'} type="text" onChange={titleChangeHandler}/>
+        <input name={'title'} value={enteredTitle} type="text" onChange={titleChangeHandler}/>
       </div>
       <div className={'new-expense__control'}>
         <label htmlFor="amount">Amount</label>
-        <input name={'amount'} type="number" min={0.01} step={0.01} onChange={amountChangeHandler}/>
+        <input name={'amount'} value={enteredAmount} type="number" min={0.01} step={0.01} onChange={amountChangeHandler}/>
       </div>
       <div className={'new-expense__control'}>
         <label htmlFor="date">Date</label>
-        <input name={'date'} type="date" min={'2019-01-01'} max={'2022-12-31'} onChange={dateChangeHandler}/>
+        <input name={'date'} value={enteredDate} type="date" min={'2019-01-01'} max={'2022-12-31'} onChange={dateChangeHandler}/>
       </div>
       <div className={'new-expense__actions'}>
         <button type={'submit'}>Add Expense</button>
