fa5a602cbeda2b8cb945524a447c2f8e8861a425
Victor <victormovileanu@gmail.com>
2022-07-20 22:00:34 +0200

    fix browser warning

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -3,8 +3,9 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
 function Expenses(props) {
-  const expenses = props.expenses.map(el => {
+  const expenses = props.expenses.map((el, idx) => {
     return <ExpenseItem
+      key={idx}
       title={el.title}
       amount={el.amount}
       date={el.date}/>
