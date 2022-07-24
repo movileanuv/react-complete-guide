d0ab6c8ae05ddc5e7ec9e8431ef23e1c21008485
Victor <victormovileanu@gmail.com>
2022-07-24 09:54:51 +0200

    fix: simplify

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -6,20 +6,20 @@ import {useState} from "react";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState('2020')
-  const [expenses, setExpenses] = useState(props.expenses)
 
   function filterExpensesHandler(year) {
     setSelectedYear(year)
-    setExpenses(props.expenses.filter(i => i.date.getFullYear() === Number(year)))
   }
 
-  let expenseItems = expenses.map((el, idx) => {
-    return <ExpenseItem
-      key={idx}
-      title={el.title}
-      amount={el.amount}
-      date={el.date}/>
-  })
+  let expenseItems = props.expenses
+    .filter(i => i.date.getFullYear() === Number(selectedYear))
+    .map((el, idx) => {
+      return <ExpenseItem
+        key={idx}
+        title={el.title}
+        amount={el.amount}
+        date={el.date}/>
+    })
 
   return (
     <div>
