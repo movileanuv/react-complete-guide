b3a952aa2fec9adbb770d01c7a2cf5b212024ff8
Victor <victor.movileanu@check24.de>
2022-12-06 09:25:11 +0100

    #18 multiple state properties

--- 18-diving-into-redux/test-redux/test.js
+++ 18-diving-into-redux/test-redux/test.js
@@ -1,18 +1,22 @@
 const redux = require("redux")
 
-function counterReducer(state = {counter: 0}, action) {
-    if (action.type === 'increment') return { counter: state.counter + 1 }
-    if (action.type === 'decrement') return { counter: state.counter - 1 }
-    if (action.type === 'increase') return { counter: state.counter + action.amount }
+const initialState = {counter: 0, showCounter: true}
+
+function counterReducer(state = initialState, action) {
+  if (action.type === 'increment') return {...state, counter: state.counter + 1}
+  if (action.type === 'decrement') return {...state, counter: state.counter - 1}
+  if (action.type === 'increase') return {...state, counter: state.counter + action.amount}
+  if (action.type === 'toggle') return {...state, showCounter: !state.showCounter}
 }
 
-const store = redux.configureStore(counterReducer)
+const store = redux.createStore(counterReducer)
 
 function counterSubscriber() {
-    const latestState = store.getState()
-    console.log(latestState)
+  const latestState = store.getState()
+  console.log(latestState)
 }
 
 store.subscribe(counterSubscriber)
 store.dispatch({type: 'increment'})
-store.dispatch({type: 'increment', amount: 5})
+store.dispatch({type: 'increase', amount: 5})
+store.dispatch({type: 'toggle'})
