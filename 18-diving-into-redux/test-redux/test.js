831c2d08957a0c65434f0dee3703a3bef184b238
Victor <victor.movileanu@check24.de>
2022-12-06 09:16:09 +0100

    #18 increase by amount

--- 18-diving-into-redux/test-redux/test.js
+++ 18-diving-into-redux/test-redux/test.js
@@ -3,6 +3,7 @@ const redux = require("redux")
 function counterReducer(state = {counter: 0}, action) {
     if (action.type === 'increment') return { counter: state.counter + 1 }
     if (action.type === 'decrement') return { counter: state.counter - 1 }
+    if (action.type === 'increase') return { counter: state.counter + action.amount }
 }
 
 const store = redux.configureStore(counterReducer)
@@ -14,3 +15,4 @@ function counterSubscriber() {
 
 store.subscribe(counterSubscriber)
 store.dispatch({type: 'increment'})
+store.dispatch({type: 'increment', amount: 5})
